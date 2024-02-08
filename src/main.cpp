fcb6b3b049cb2ed51503cb964dba14c5437194d2
Aidan Sharp <aidan.sharp@siarune.dev>
2024-02-08 11:24:36 -0800

    Add motors
    Instantiate motors and motor group for movement.

--- src/main.cpp
+++ src/main.cpp
@@ -8,55 +8,26 @@
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
-
 using namespace vex;
 
-// A global instance of competition
 competition Competition;
+motor ML = motor(PORT1, ratio18_1);
+motor MR = motor(PORT2, ratio18_1);
 
-// define your global instances of motors and other devices here
+motor_group M = motor_group(ML,MR);
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+  return;
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-}
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+  M.spin(forward);
+
+  
+}
 
 void usercontrol(void) {
   // User control code here, inside the loop
@@ -75,9 +46,6 @@ void usercontrol(void) {
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
 int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
@@ -85,6 +53,7 @@ int main() {
 
   // Run the pre-autonomous function.
   pre_auton();
+  autonomous();
 
   // Prevent main from exiting with an infinite loop.
   while (true) {
